d4b9909f99ddaaf3f8c53f230e5399d4f6528c3e
DEV KELVIN <DEV@ALP09ITDVO014.isupportint.local>
2026-03-31 18:05:48 +0800

    [ADD] dispatch candidate feature

--- addons/hr_recruitment/models/hr_applicant.py
+++ addons/hr_recruitment/models/hr_applicant.py
@@ -113,6 +113,8 @@ class Applicant(models.Model):
     applicant_properties = fields.Properties('Properties', definition='job_id.applicant_properties_definition', copy=True)
     applicant_notes = fields.Html()
     refuse_date = fields.Datetime('Refuse Date')
+    dispatch_date = fields.Datetime('Dispatch Date', store=True)
+    is_dispatched = fields.Boolean('Is Dispatched', store=True)
 
     def init(self):
         super().init()
